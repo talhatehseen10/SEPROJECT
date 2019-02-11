b2d13fa1a6fe45d37fba4ebee44416ea9500ddf1
MansoorAhmedMesso <47540679+MansoorAhmedMesso@users.noreply.github.com>
2019-02-12 01:53:01 +0500

    Update Data.java

--- Data.java
+++ Data.java
@@ -3,14 +3,21 @@ class Data{
 		java.util.Scanner in = new java.util.Scanner(System.in);
 		System.out.println("Talha's ID is 11");
 		System.out.println("Sohail's ID is 12");
+		System.out.print("Mansoor's ID is 13");
 		System.out.print("Enter Student ID:");
 		int id = in.nextInt();
 			if (id == 11){
-			String name = "Talha";
-			String rollno = "2k17/csee/75";
-			System.out.println("Name:"+name);
-			System.out.println("Roll no is:"+rollno);
+				String name = "Talha";
+				String rollno = "2k17/csee/75";
+				System.out.println("Name:"+name);
+				System.out.println("Roll no is:"+rollno);
 			}		
+			if (id == 13){
+				String name = "Mansoor";
+				String rollno = "2k17/csme/09";
+				System.out.println("Name:"+name);
+				System.out.println("Roll no is:"+rollno);
+			}
 	}
 	public static void main(String str[]){
 		Data d = new Data();
