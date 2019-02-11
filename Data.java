12dfe8f32126462fb22cdd1f92eed3f70a7180f2
talhatehseen10 <47538883+talhatehseen10@users.noreply.github.com>
2019-02-12 01:14:50 +0500

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,19 @@
+class Data{
+	void studentData(){
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		System.out.println("Talha's ID is 11");
+		System.out.println("Sohail's ID is 12");
+		System.out.print("Enter Student ID:");
+		int id = in.nextInt();
+			if (id == 11){
+			String name = "Talha";
+			String rollno = "2k17/csee/75";
+			System.out.println("Name:"+name);
+			System.out.println("Roll no is:"+rollno);
+			}		
+	}
+	public static void main(String str[]){
+		Data d = new Data();
+		d.studentData();	
+	}	
+}
