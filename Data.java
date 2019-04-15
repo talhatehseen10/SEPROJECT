64ed60f583c831aac51d669043fd517fdb5d2f99
sohail124 <47071815+sohail124@users.noreply.github.com>
2019-04-16 01:51:57 +0500

    Adding Student Data.

--- Data.java
+++ Data.java
@@ -10,7 +10,15 @@ class Data{
 			String rollno = "2k17/csee/75";
 			System.out.println("Name:"+name);
 			System.out.println("Roll no is:"+rollno);
+			}
+			
+		        if (id == 12){
+			String name = "Sohail";
+			String rollno = "2k17/csee/74";
+			System.out.println("Name:"+name);
+			System.out.println("Roll no is:"+rollno);
 			}		
+		
 	}
 	public static void main(String str[]){
 		Data d = new Data();
